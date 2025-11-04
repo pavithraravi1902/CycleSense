e5fa9d7f8215d63fac49138c9f8ae57c5d9e4e74
Pavithra R <pavithrar@smartek21.com>
2025-11-04 14:42:16 +0530

    backend setup

---
+++ backend/src/test/java/com/cyclesense/backend/CycleSenseBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cyclesense.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CycleSenseBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
